1893615519f8f50a137712d5fe302b35e614da0d
Akshat Jain <jain.akshat31@gmail.com>
2020-05-27 12:03:17 +0530

    Comments added

--- plot1.R
+++ plot1.R
@@ -7,6 +7,6 @@ electric_data$Date <- as.Date(electric_data$Date, format = "%d/%m/%Y")
 working_data <- filter(electric_data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
 # plotting the graph into plot1.png
-png("plot1.png")
+png("plot1.png") #default height and width are 480 pixels so no need to pass them as arguments. 
 with(working_data, hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col="red"))
 dev.off()
